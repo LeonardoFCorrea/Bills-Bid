42c9cc58b97402995c50f50cb345b2677a90f4dd
Leonardo Félix Corrêa <leonardofelixcorrea@gmail.com>
2022-10-19 14:36:14 -0300

    [Front-end] FIX Dashboard page style

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -39,16 +39,18 @@ class _DashboardState extends State<DashboardPage> {
                         color: Color(0xFF607C4B),
                         borderRadius: BorderRadius.circular(18)),
                     child: Padding(
-                      padding: EdgeInsets.only(
-                          left: 13, right: 13, top: 5, bottom: 5),
+                      padding: EdgeInsets.only(left: 13, right: 13, bottom: 5),
                       child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text(
-                              'YOUR BALANCE',
-                              style: TextStyle(
-                                fontSize: 15,
-                                color: Colors.white,
+                            Padding(
+                              padding: EdgeInsets.only(top: 5),
+                              child: Text(
+                                'YOUR BALANCE',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  color: Colors.white,
+                                ),
                               ),
                             ),
                             Container(
@@ -81,38 +83,46 @@ class _DashboardState extends State<DashboardPage> {
                 ),
                 Padding(
                   padding: EdgeInsets.only(bottom: 34),
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        PageTransition(
-                          type: PageTransitionType.rightToLeftWithFade,
-                          duration: Duration(milliseconds: 550),
-                          child: DashboardPage(),
-                          childCurrent: DashboardPage(),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      height: 180,
-                      width: 180,
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Color(0xFF7ED936),
-                            width: 5,
-                          ),
-                          borderRadius: BorderRadius.circular(100),
-                          color: Color(0xFF607C4B)),
-                      child: Padding(
-                        padding: EdgeInsets.only(top: 72, left: 19, right: 19),
-                        child: Text(
-                          'ADD FUNDS',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 24,
+                  child: Container(
+                    child: Material(
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            PageTransition(
+                              type: PageTransitionType.rightToLeftWithFade,
+                              duration: Duration(milliseconds: 550),
+                              child: DashboardPage(),
+                              childCurrent: DashboardPage(),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          height: 180,
+                          width: 180,
+                          child: Padding(
+                            padding:
+                                EdgeInsets.only(top: 74, left: 24, right: 19),
+                            child: Text(
+                              'ADD FUNDS',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24,
+                              ),
+                            ),
                           ),
                         ),
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      color: Colors.transparent,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      color: Color(0xFF607C4B),
+                      border: Border.all(
+                        color: Color(0xFF7ED936),
+                        width: 5,
                       ),
                     ),
                   ),
@@ -127,9 +137,214 @@ class _DashboardState extends State<DashboardPage> {
                       topRight: Radius.circular(30),
                     ),
                   ),
+                  padding: EdgeInsets.only(left: 30, top: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '14 SEP 2022',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Color(0xFF5C3F06),
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 21,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Image.asset('images/green-card.png'),
+                              Padding(
+                                padding: EdgeInsets.only(left: 7),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Bills',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                    Text(
+                                      'Heimstaden SA',
+                                      style: TextStyle(
+                                        fontSize: 13,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(right: 30),
+                            child: Text(
+                              '-\$1230',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Image.asset('images/green-card.png'),
+                              Padding(
+                                padding: EdgeInsets.only(left: 7),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Pokemon Card',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                    Text(
+                                      'Pokémon Center, NY',
+                                      style: TextStyle(
+                                        fontSize: 13,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(right: 30),
+                            child: Text(
+                              '-\$372',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Image.asset('images/green-card.png'),
+                              Padding(
+                                padding: EdgeInsets.only(left: 7),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Fire Wings',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                    Text(
+                                      'Walmart',
+                                      style: TextStyle(
+                                        fontSize: 13,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(right: 30),
+                            child: Text(
+                              '+\$135',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 11,
+                      ),
+                      Text(
+                        '14 SEP 2022',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Color(0xFF5C3F06),
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Image.asset('images/green-card.png'),
+                              Padding(
+                                padding: EdgeInsets.only(left: 7),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Noogler Salary',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                    Text(
+                                      'Google',
+                                      style: TextStyle(
+                                        fontSize: 13,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(right: 30),
+                            child: Text(
+                              '+\$2830',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
                 Padding(
-                  padding: EdgeInsets.only(top: 16, bottom: 16),
+                  padding: EdgeInsets.only(top: 11, bottom: 10),
                   child: Container(
                     height: 45,
                     width: 309,
