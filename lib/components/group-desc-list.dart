0298cef4caee1985075c7ae9552557d816ec27cf
Leonardo Félix Corrêa <leonardofelixcorrea@gmail.com>
2022-11-15 20:33:49 -0300

    [Front-end] ADD GroupDescription Page

--- lib/components/group-desc-list.dart
+++ lib/components/group-desc-list.dart
@@ -9,45 +9,87 @@ class GDescList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(left: 40, right: 40),
-      child: Container(
-          height: 50,
-          width: 300,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(
-              color: Color(0xff7BC144),
-            ),
-          ),
-          child: Padding(
-            padding: EdgeInsets.only(left: 16, right: 13, top: 3, bottom: 3),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Image.asset("images/grupo-teste.png", width: 50),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      "$nome",
-                      style: TextStyle(
+        padding: EdgeInsets.only(left: 40, right: 40),
+        child: Container(
+          child: Material(
+            child: InkWell(
+              onTap: () => showDialog<String>(
+                context: context,
+                builder: (BuildContext context) => AlertDialog(
+                  backgroundColor: Color(0xff559E1C),
+                  contentPadding: EdgeInsets.all(0),
+                  buttonPadding: EdgeInsets.all(0),
+                  title: Text(
+                    'Remove $nome',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.normal),
+                  ),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () => Navigator.pop(context, 'Cancel'),
+                      child: Text(
+                        'Cancel',
+                        style: TextStyle(
+                          color: Colors.white,
                           fontSize: 14,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () => Navigator.pop(context, 'Remove'),
+                      child: Text(
+                        'Remove',
+                        style: TextStyle(
+                          color: Color(0xff7BC144),
+                          fontSize: 14,
+                        ),
+                      ),
                     ),
                   ],
                 ),
-                Container(
-                  width: 15,
-                  height: 15,
-                  decoration:
-                      BoxDecoration(shape: BoxShape.circle, color: color),
-                )
-              ],
+              ),
+              child: Container(
+                height: 50,
+                width: 300,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: Color(0xff7BC144),
+                  ),
+                ),
+                child: Padding(
+                  padding:
+                      EdgeInsets.only(left: 16, right: 13, top: 3, bottom: 3),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Image.asset("images/grupo-teste.png", width: 50),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "$nome",
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        width: 15,
+                        height: 15,
+                        decoration:
+                            BoxDecoration(shape: BoxShape.circle, color: color),
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ),
-          )),
-    );
+          ),
+        ));
   }
 }
